9214b25a183e1287645b3f2164640917aee9455a
DeveloperAlan <alanng95@hotmail.com>
2019-01-31 13:30:18 +1000

    finished up exercise one

--- app/router.js
+++ app/router.js
@@ -10,11 +10,13 @@ Router.map(function() {
   this.route('orgs'); // orgs
   this.route('org', {path: 'org/:id'}, function() { //org/emberjs
     this.route('repos');
-    this.route('repo', {path: ':id'}, function() {
+    this.route('repo', {path: ':repoid'}, function() {
       this.route('contributors');
       this.route('issues');
     }); // org/jquery/jquery-ui
   });
+
+  this.route('notfound', {path: '*path'});
 });
 
 export default Router;
