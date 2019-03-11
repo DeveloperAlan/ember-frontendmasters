4c8eb12c27b754e47606f4884076909d8e0f7730
alan <alanng95@hotmail.com>
2019-03-11 22:42:17 +1100

    added the repos to the repo page

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    model(params) {
+        let orgId = Ember.get(this.modelFor('org'), 'login');
+        return $.get(`https://api.github.com/orgs/${orgId}/repos?access_token=b6140a7eea94c4fe2727ab4a09d334da3fdc817c`).then((result) => {
+            return result.map(repo => {
+                repo.oldId = repo.id;
+                repo.id = repo.name;
+                return repo;
+            })
+        })
+    }
 });
