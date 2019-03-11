b3fce2010c5a833c7a04b77543ee443e22542df5
alan <alanng95@hotmail.com>
2019-03-11 22:31:18 +1100

    got the model working on the template via github

--- app/routes/org.js
+++ app/routes/org.js
@@ -2,8 +2,10 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
     model(params) {
-        $.get(`https://api.github.com/orgs/${params.id}?access_token=b6140a7eea94c4fe2727ab4a09d334da3fdc817c`).then((result) => {
-            console.log(result);
+        return $.get(`https://api.github.com/orgs/${params.id}?access_token=b6140a7eea94c4fe2727ab4a09d334da3fdc817c`).then((result) => {
+            result.oldId = result.id;
+            result.id = result.name;
+            return result;
         })
     }
 });
